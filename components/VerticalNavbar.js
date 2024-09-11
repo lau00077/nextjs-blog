4d05a2e9cf775e63a964c8fc456d0d93bbef16bf
Meiyi Lau <lau00077@algonquinlive.com>
2024-09-11 18:24:44 -0400

    Updates pages

---
+++ components/VerticalNavbar.js
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+const VerticalNavbar = () => {
+  return (
+    <div className="fixed right-6 top-0 h-full flex flex-col items-center text-gray-400 w-20">
+      <ul className="flex flex-col items-center mt-10 space-y-6">
+        <li>
+          <a href="#about" className="hover:text-gray-700">About</a>
+        </li>
+        <li>
+          <a href="#services" className="hover:text-gray-700">Services</a>
+        </li>
+        <li>
+          <a href="#works" className="hover:text-gray-700">Works</a>
+        </li>
+        <li>
+          <a href="#contact" className="hover:text-gray-700">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default VerticalNavbar;
--- pages/index.js
+++ pages/index.js
@@ -1,114 +1,199 @@
+import Head from "next/head";
+import {
+  AiFillLinkedin,
+  AiFillGithub,
+} from "react-icons/ai";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { useState } from "react";
+import VerticalNavbar from "./components/VerticalNavbar";
+import avatar from "../public/avatar.png";
+import code from "../public/code.png";
+import design from "../public/design.png";
+import consulting from "../public/consulting.png";
 import Image from "next/image";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import web1 from "../public/web1.png";
+import web2 from "../public/web2.png";
+import web3 from "../public/web3.png";
+import web4 from "../public/web4.png";
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [darkMode, setDarkMode] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  return (
+    <div className={darkMode ? "dark" : ""}>
+      <Head>
+        <title>Faye Lau</title>
+        <meta name="description" content="My Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <VerticalNavbar />
+      <main id="about" className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between dark:text-white">
+            <h1 className="font-burtons text-xl">Faye Lau</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className=" cursor-pointer text-2xl"
+                />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-red-400 text- to-orange-400 text-white px-4 py-2 border-none rounded-md ml-8"
+                  href="https://drive.google.com/file/d/1jP9J05z-f7sOU9DCHDCBRolegnloaA0K/view?usp=sharing"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10 py-10">
+            <h2 className="text-5xl py-2 text-orange-400 font-medium dark:text-orange-200 md:text-6xl">
+              Faye, Meiyi Lau
+            </h2>
+            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+              Web Designer & Event Specialist
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-500 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+            Based in Canada, chasing simplicity as the essence of elegance in digital experiences.
+            </p>
+            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-200">
+            <a href="https://www.linkedin.com/in/fayelau/" target="_blank" rel="noopener noreferrer">
+              <AiFillLinkedin />
+            </a>
+
+            <a href="https://github.com/lau00077" target="_blank" rel="noopener noreferrer">
+              <AiFillGithub />
+            </a>
+
+            </div>
+            <div className="mx-auto bg-gradient-to-b from-orange-200 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+              <Image src={avatar} layout="fill" objectFit="cover" />
+            </div>
+          </div>
+        </section>
+        <section>
+          <div id="services">
+            <h3 className="text-3xl py-1 dark:text-white ">SERVICES</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+            I’ve been freelancing as a web designer and developer, working remotely with agencies, startups, and awesome people to create cool digital products. If you need something designed, developed, or managed, I’m your go-to!
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              I offer from a wide range of services, including <span className="text-red-300"> Event Managememt, Web Design, and Photography.</span>
+               
+            </p>
+          </div>
+          <div className="lg:flex gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
+              <Image src={design} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2  ">
+                Event Management &<br />Digital Marketing
+              </h3>
+              <p className="py-2">
+                Seasoned event manager specializing in trade shows and conference planning, with a knack for savvy digital marketing strategies.
+              </p>
+              <h4 className="py-4 text-orange-400">Digital Marketing Tools</h4>
+              <p className="text-gray-500 py-1">Google Analytics</p>
+              <p className="text-gray-500 py-1">Google Search Console</p>
+              <p className="text-gray-500 py-1">Moz</p>
+              <p className="text-gray-500 py-1">Mailchimp</p>
+              <p className="text-gray-500 py-1">HubSpot</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+              <Image src={code} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2 ">
+                Web & UX/ UI Design
+              </h3>
+              <p className="py-2">
+                Designing websites that don’t just look great but work like a charm.
+              </p>
+              <h4 className="py-4 text-orange-400">Design Tools</h4>
+              <p className="text-gray-500 py-1">Photoshop</p>
+              <p className="text-gray-500 py-1">Illustrator</p>
+              <p className="text-gray-500 py-1">Figma</p>
+              <p className="text-gray-500 py-1">Indesign</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+              <Image src={consulting} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2 ">Photography</h3>
+              <p className="py-2">
+              Need awesome photos? I’ll capture everything from your big events to family smiles or the next best product shot, making sure you look your absolute best (even on a Monday).
+              </p>
+              <h4 className="py-4 text-orange-400">Photography Types</h4>
+              <p className="text-gray-500 py-1">Corporate Events</p>
+              <p className="text-gray-500 py-1">Parties</p>
+              <p className="text-gray-500 py-1">Portraits</p>
+              <p className="text-gray-500 py-1">Graduation</p>
+            </div>
+          </div>
+        </section>
+        <section className="py-10">
+          <div id="works">
+            <h3 className="text-3xl py-1 dark:text-white ">WORKS</h3>
+          </div>
+          <div className="flex flex-col gap-5 py-8 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1 ">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                objectFit="contain"
+                src={web1}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                objectFit="contain"
+                src={web2}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                objectFit="contain"
+                src={web3}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                objectFit="contain"
+                src={web4}
+              />
+            </div>
+          </div>
+        </section>
+        <section>
+          <div id="contact">
+            <h3 className="text-3xl py-1 dark:text-white ">CONTACT</h3>
+
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+            Whether you’re looking to start a new project, need help with an existing one, or just want to say hello, feel free to get in touch.</p>
+
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Email: <span />
+              <a href="mailto:fayelmy@gmail.com" className="text-red-300  inline-block py-1"> fayelmy@gmail.com</a>
+            </p>
+          
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer>
+        <div className="bg-gray-800 text-white text-center py-5">
+          <p>© 2024 Faye Lau. All rights reserved.</p>
+        </div>
       </footer>
     </div>
   );
